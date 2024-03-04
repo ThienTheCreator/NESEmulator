959582cd756c526a04344000408db28098a69165
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2024-03-04 00:13:23 -0800

    architecture of cpu and window for win32

--- Nes/Nes.cpp
+++ Nes/Nes.cpp
@@ -3,17 +3,52 @@
 
 using namespace std;
 
-class Nes {
+class MOS_6502 {
 	// CPU registers
 	uint8_t a;   // arithmetic logic unit (ALU)
 	uint8_t x;   // index x
 	uint8_t y;   // index y
 	uint16_t pc; // program counter
 	uint8_t s;   // stack pointer
+	
+	/* 
+	7  bit  0
+	---- ----
+	NV1B DIZC
+	|||| ||||
+	|||| |||+- Carry
+	|||| ||+-- Zero
+	|||| |+--- Interrupt Disable
+	|||| +---- Decimal
+	|||+------ (No CPU effect; see: the B flag)
+	||+------- (No CPU effect; always pushed as 1)
+	|+-------- Overflow
+	V+--------- Negative
+
+	Diagram from https://www.nesdev.org/wiki/Status_flags
+	*/
 	uint8_t p;   // status register
 
-	uint8_t cRAM[65536]; // CPU memory
+	void instructions(opcode){
+		// BRK - Force Interrupt
+		if(opcode == 0x00){
+			mem[s] = pc >> 8;
+			s++;
+			mem[s] = (uint8_t)pc;
+			s++;
+			mem[s] = p;
+			s++;
+			s = s | 0b00010000;
+		}
+	}
 };
+
+class Bus{
+	uint8_t memory[65536];
+};
+
+MOS_6502 cpu;
+Bus bus;
 
 int main() {
 	return 0;
---
+++ window/window.cpp
@@ -0,0 +1,120 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void getidle();
+
+HWND wind = NULL;
+int test = 0;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR pCmdLine, int nCmdShow)
+{
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+	WNDCLASS wc = { };
+
+	wc.lpfnWndProc   = WindowProc;
+	wc.hInstance     = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	// Create the window.
+	
+	wind = CreateWindowEx(
+		0,
+		CLASS_NAME,
+		L"Learn to Program Windows",
+		WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU,
+
+		// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, 256, 240,
+
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+		);
+
+	if (wind == NULL)
+	{
+		return 0;
+	}
+	
+	ShowWindow(wind, nCmdShow);
+
+	
+    SetTimer(wind, 1, 200, (TIMERPROC) getidle);
+	
+	// Run the message loop.
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+void getidle(){
+	InvalidateRect(wind, 0, TRUE);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+			HDC hdcMem = CreateCompatibleDC(hdc);
+
+			int w = 256;
+			int h = 240;
+
+			int32_t* pvBits = NULL;
+
+			BITMAPINFO bmi;
+			memset(&bmi, 0, sizeof(BITMAPINFO));
+			bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+			bmi.bmiHeader.biWidth = 256;
+			bmi.bmiHeader.biHeight = 240;
+			bmi.bmiHeader.biPlanes = 1;
+			bmi.bmiHeader.biBitCount = 32;
+			bmi.bmiHeader.biCompression = BI_RGB;
+			
+			HBITMAP bitmap = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, (void**)&pvBits, 0, 0);
+			HGDIOBJ oldbmp = SelectObject(hdcMem, bitmap); 
+
+			BitBlt(hdcMem, 0, 0, w, h, hdc, 0, 0, SRCCOPY);
+			
+			for(int i = 0; i < w*h; i++){
+				pvBits[i] = 0x00000000;
+			}
+
+			BitBlt(hdc, 0, 0, w, h, hdcMem, 0, 0, SRCCOPY);
+
+			SelectObject(hdcMem, oldbmp);
+			DeleteObject(bitmap);
+			DeleteDC(hdcMem);
+
+			EndPaint(hwnd, &ps);
+
+			return 0;
+		}
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+	}
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
